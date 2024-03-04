d78504a729da3a2535ad6a6b7ffaa34a62d57e74
its-me2h <harmouchet@gmail.com>
2024-03-04 10:53:34 +0100

    [DEV-8] Add the User schema

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import apolloConfig from './src/configs/apollo';
-import gql from 'graphql-tag';
+import typeDefs from './src/graphql/schema'
 
 const app = express();
 app.use(express.json());
 
 const apollo = new ApolloServer({
     ...apolloConfig,
-    // schema: buildSubgraphSchema([{ typeDefs, resolvers }])
+    schema: buildSubgraphSchema([{ typeDefs }])
 });
 
 apollo.start()

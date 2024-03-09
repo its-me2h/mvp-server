cc717481b23ac5cf624452eb76aedce36d766e92
its-me2h <harmouchet@gmail.com>
2024-03-09 20:32:20 +0100

    [DEV-26] Implement resolvers for the construction schema

---
+++ src/graphql/resolvers/constructions/Complex.resolvers.ts
@@ -0,0 +1,27 @@
+import { v4 as uuid } from 'uuid';
+import { Complex } from '../../../models';
+
+export const Query = {
+    // Resolver function to get Complex by ID
+    complex: async (_: any, { id }: { id: string }) => {
+        return await Complex.findByPk(id);
+    },
+};
+
+export const Mutation = {
+    // Resolver function to create a new Complex
+    createComplex: async (_: any, { object }: { object: any }) => {
+        return await Complex.create({ id: uuid(), ...object });
+    },
+    // Resolver function to update the Complex by ID
+    updateComplex: async (_: any, { id, object }: { id: string, object: any }) => {
+        const complex = await Complex.findByPk(id);
+        // If Complex is not found, throw an error
+        if (!complex) {
+            throw new Error('Complex not found');
+        }
+        // Update the Complex with the provided object
+        await complex.update(object);
+        return complex;
+    },
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,7 @@ export { User } from "./users/User.model";
 export { Admin } from "./users/Admin.model";
 export { Manager } from "./users/Manager.model";
 export { Client } from "./users/Client.model";
+
+export { Complex } from "./constructions/Complex.model";
+export { Block } from "./constructions/Block.model";
+export { Apartment } from "./constructions/Apartment.model";

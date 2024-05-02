e9f045c95f35516e681d04ddf255441b8c1e1bf6
its-me2h <harmouchet@gmail.com>
2024-05-02 20:20:13 +0100

    [DEV-79] Apply each directive transformer to the schema

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,14 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
+import { directiveTransformers } from './directives';
 
-// Create a executable GraphQL schema
-export const schema = makeExecutableSchema({ typeDefs, resolvers });
+// Create an executable GraphQL schema
+let schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Apply directive transformers to the schema
+directiveTransformers.forEach(({ transformer, name }) => {
+    schema = transformer(schema, name);
+});
+
+export { schema };

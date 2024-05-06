be2594d940e0a414eb91db9dee6368e3488de925
its-me2h <harmouchet@gmail.com>
2024-05-06 15:01:03 +0100

    [DEV-90] Update Client Model Schema and Resolvers

--- src/user-service/graphql/resolvers/client.ts
+++ src/user-service/graphql/resolvers/client.ts
@@ -2,15 +2,7 @@ import { Client } from '../../models';
 
 export const Mutation = {
     // Resolver function to create an account
-    createClient: async (_: any, { object }: { object: any }) => {
-        return await Client.create({
-            ...object
-        });
-    },
-    // Resolver function to update the Client by ID
-    updateClient: async (_: any, { accountId, object }: { accountId: string, object: any }) => {
-        const client = await Client.findByPk(accountId);
-        if (!client) throw new Error('Client not found');
-        return client.update(object);
+    createClient: async (_: any, { input }: { input: any }) => {
+        return await Client.create(input);
     },
 };
--- src/user-service/models/client.ts
+++ src/user-service/models/client.ts
@@ -2,9 +2,18 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../../../configs/sequelize';
 
 export const Client = sequelize.define('Client', {
-    accountId: {
+    id: {
         type: DataTypes.STRING,
         primaryKey: true,
+    },
+    userId: {
+        type: DataTypes.STRING,
+    },
+    complexId: {
+        type: DataTypes.STRING,
+    },
+    blockId: {
+        type: DataTypes.STRING,
     },
     apartmentId: {
         type: DataTypes.STRING,
